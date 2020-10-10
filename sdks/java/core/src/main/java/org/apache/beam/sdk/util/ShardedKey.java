925b27a38305a3aff18f0d1edd4583d43ae20fc4
sychen <sychen@google.com>
2020-10-09 18:04:52 -0700

    Add a well-known ShardedKey coder in Java SDK

---
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/ShardedKey.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.util;
+
+import com.google.auto.value.AutoValue;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.List;
+import javax.annotation.Nullable;
+import org.apache.beam.sdk.coders.BooleanCoder;
+import org.apache.beam.sdk.coders.ByteArrayCoder;
+import org.apache.beam.sdk.coders.StructuredCoder;
+import org.apache.beam.sdk.util.common.ElementByteSizeObserver;
+
+/**
+ * A sharded key consisting of a user key and a shard id represented by bytes.
+ *
+ * <p>This is a more generic definition of {@link org.apache.beam.sdk.values.ShardedKey}.
+ */
+@AutoValue
+public abstract class ShardedKey<K> {
+
+  public static <K> ShardedKey<K> of(K key, @Nullable byte[] shardId) {
+    return new AutoValue_ShardedKey(shardId, key);
+  }
+
+  @SuppressWarnings("mutable")
+  @Nullable
+  public abstract byte[] getShardId();
+
+  public abstract K getKey();
+
+  public static class Coder<K> extends StructuredCoder<ShardedKey<K>> {
+
+    private final ByteArrayCoder shardCoder = ByteArrayCoder.of();
+    private final org.apache.beam.sdk.coders.Coder<K> keyCoder;
+
+    private Coder(org.apache.beam.sdk.coders.Coder<K> coder) {
+      keyCoder = coder;
+    }
+
+    public static <K> ShardedKey.Coder<K> of(org.apache.beam.sdk.coders.Coder<K> keyCoder) {
+      return new ShardedKey.Coder<K>(keyCoder);
+    }
+
+    public org.apache.beam.sdk.coders.Coder<K> getKeyCoder() {
+      return keyCoder;
+    }
+
+    @Override
+    public void encode(ShardedKey<K> shardedKey, OutputStream outStream) throws IOException {
+      // The encoding should follow the order:
+      //   null indicator
+      //   length of shard id
+      //   shard id
+      //   encoded user key
+      BooleanCoder.of().encode(shardedKey.getShardId() != null, outStream);
+      if (shardedKey.getShardId() != null) {
+        shardCoder.encode(shardedKey.getShardId(), outStream);
+      }
+      keyCoder.encode(shardedKey.getKey(), outStream);
+    }
+
+    @Override
+    public ShardedKey<K> decode(InputStream inStream) throws IOException {
+      if (!BooleanCoder.of().decode(inStream)) {
+        K key = keyCoder.decode(inStream);
+        return ShardedKey.of(key, null);
+      }
+      byte[] shardId = shardCoder.decode(inStream);
+      K key = keyCoder.decode(inStream);
+      return ShardedKey.of(key, shardId);
+    }
+
+    @Override
+    public List<? extends org.apache.beam.sdk.coders.Coder<?>> getCoderArguments() {
+      return Collections.singletonList(keyCoder);
+    }
+
+    @Override
+    public List<? extends org.apache.beam.sdk.coders.Coder<?>> getComponents() {
+      return Collections.singletonList(keyCoder);
+    }
+
+    @Override
+    public void verifyDeterministic() throws NonDeterministicException {
+      verifyDeterministic(this, "Key coder must be deterministic", keyCoder);
+    }
+
+    @Override
+    public void registerByteSizeObserver(ShardedKey<K> shardedKey, ElementByteSizeObserver observer)
+        throws Exception {
+      BooleanCoder.of().registerByteSizeObserver(shardedKey.getShardId() != null, observer);
+      if (shardedKey.getShardId() != null) {
+        shardCoder.registerByteSizeObserver(shardedKey.getShardId(), observer);
+      }
+      keyCoder.registerByteSizeObserver(shardedKey.getKey(), observer);
+    }
+  }
+}
